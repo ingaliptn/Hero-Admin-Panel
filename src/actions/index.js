2362e1a909956ea07ae1aff3464ddd437f43972f
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-05-31 13:31:54 +0300

    Refactor by using ReduxThunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,17 @@
+export const fetchHeroes = (request) => (dispatch) => {
+  dispatch(heroesFetching());
+    request("http://localhost:3001/heroes")
+      .then((data) => dispatch(heroesFetched(data)))
+      .catch(() => dispatch(heroesFetchingError()));
+}
+
+export const fetchFilters = (request) => (dispatch) => {
+  dispatch(filtersFetching());
+    request("http://localhost:3001/filters")
+      .then((data) => dispatch(filtersFetched(data)))
+      .catch(() => dispatch(filtersFetchingError()));
+}
+
 export const heroesFetching = () => {
   return {
     type: "HEROES_FETCHING",
@@ -36,11 +50,13 @@ export const filtersFetchingError = () => {
   };
 };
 
-export const activeFilterChanged = (filter) => {
-  return {
-    type: "ACTIVE_FILTER_CHANGED",
-    payload: filter,
-  };
+export const activeFilterChanged = (filter) => (dispatch) => {
+  setTimeout(() => {
+    dispatch({
+      type: "ACTIVE_FILTER_CHANGED",
+      payload: filter,
+    });
+  }, 300);
 };
 
 export const heroCreated = (hero) => {
